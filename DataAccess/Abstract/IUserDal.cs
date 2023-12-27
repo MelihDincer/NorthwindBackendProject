7edc539405438a3188659f17fa36d9c084e51458
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-27 19:01:00 +0300

    IUserDal ın oluşturulması.

---
+++ DataAccess/Abstract/IUserDal.cs
@@ -0,0 +1,17 @@
+﻿using Core.DataAccess;
+using Core.Entities.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        void Add(User user);
+
+        //Parametre olarak verilen kullanıcının rollerini çekmek için imza
+        List<OperationClaim> GetClaims(User user);
+    }
+}
