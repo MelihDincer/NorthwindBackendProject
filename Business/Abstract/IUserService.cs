5038847fca0b2ccf5fbfebf860feee5408286b04
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-27 18:57:03 +0300

    IUserService in oluşturulması.

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        //Kullanıcının rollerini listeleme
+        List<OperationClaim> GetClaims(User user);
+        //Yeni kullanıcı oluşturma
+        void Add(User user);
+        //Kullanıcıyı mail bilgisi ile bulma
+        User GetByMail(string mail);
+    }
+}
