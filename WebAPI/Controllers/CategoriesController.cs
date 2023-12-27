cf8d20f9f92878565cb3b0cd467628eacbc95458
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-27 19:03:02 +0300

    Categories Api Controller ının oluşturulması.

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetList() 
+        { 
+            var result = _categoryService.GetList();
+            if(result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
