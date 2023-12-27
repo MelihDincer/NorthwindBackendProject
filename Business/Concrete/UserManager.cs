1ee0ca02abbbe610ac27c059cfe2b9442f0bbb33
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-27 18:57:19 +0300

    UserManager in oluşturulması.

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void Add(User user)
+        {
+            _userDal.Add(user);
+        }
+
+        public User GetByMail(string mail)
+        {
+            return _userDal.Get(u => u.Email == mail);
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+            return _userDal.GetClaims(user);
+        }
+    }
+}
