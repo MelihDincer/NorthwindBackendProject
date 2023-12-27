f2699cde6c17f2c334ca87986eb0f1e91ba4459f
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-27 19:00:42 +0300

    Yeni dbsetlerin eklenmesi.

--- DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -1,8 +1,6 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework.Contexts
 {
@@ -15,5 +13,9 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
         }
         
         public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
     }
 }
