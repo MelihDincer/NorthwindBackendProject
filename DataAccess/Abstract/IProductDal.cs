bef26d32702aae1ba198ea65d88a1c59b17acd29
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-27 19:01:50 +0300

    Kod revize işlemi.

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal:IEntityRepository<Product>
+    public interface IProductDal : IEntityRepository<Product>
     {
     }
 }
