90b0638f3a4f8e68cebd0aecfa5fc64dc21c8777
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-28 22:27:24 +0300

    IAuthService interfaceinin oluşturulması ve authentication durumlarının businessda kontrol edilmesi.

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    //Sisteme login ya da register olma durumlarında yapılacak bazı işlemler
+    public interface IAuthService
+    {
+        //Register işlemi
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password);
+        //Login işlemi
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+
+        //Kullanıcı var mı diye kontrol işlemi
+        IResult UserExists(string email);
+
+        //AccessToken üretme işlemi
+        IDataResult<AccessToken> CreateAccessToken(User user);
+    }
+}
