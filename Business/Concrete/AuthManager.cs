f874226216ca12c2f083a9b7233ec93b90cac60c
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-28 22:27:47 +0300

    AuthService classının oluşturulması ve authentication durumlarının businessda kontrol edilecek kodların yazılması.

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,78 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Entities.Concrete;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Hashing;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private IUserService _userService;
+        ITokenHelper _tokenHelper;
+
+        public AuthManager(IUserService userService, ITokenHelper tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+
+        //Access Token Oluşturma
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            var claims = _userService.GetClaims(user);
+            var accessToken = _tokenHelper.CreateToken(user, claims);
+            return new SuccessDataResult<AccessToken>(accessToken,Messages.AccessTokenCreated);
+        }
+
+        //Login İşlemi
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            var userToCheck = _userService.GetByMail(userForLoginDto.Email); //Kontrol edilecek kullanıcı
+            //Böyle bir kullanıcı var mı?
+            if(userToCheck == null)
+            {
+                return new ErrorDataResult<User>(Messages.UserNotFound);
+            }
+            if(!HashingHelper.VerifyPasswordHash(userForLoginDto.Password, userToCheck.PasswordHash, userToCheck.PasswordSalt))
+            {
+                return new ErrorDataResult<User>(Messages.PasswordError);
+            }
+
+            return new SuccessDataResult<User>(userToCheck, Messages.SuccessfulLogin);
+        }
+
+        //Register İşlemi
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
+            var user = new User
+            {
+                Email = userForRegisterDto.Email,
+                FirstName = userForRegisterDto.FirstName,
+                LastName = userForRegisterDto.LastName,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Status = true
+            };
+            _userService.Add(user);
+            return new SuccessDataResult<User>(user, Messages.UserRegistred);
+        }
+
+        //Kullanıcının Mevcut Olma Durumu
+        public IResult UserExists(string email)
+        {
+            if(_userService.GetByMail(email) != null)
+            {
+                return new ErrorResult(Messages.UserAlreadyExists); //Kullanıcı zaten mevcut.
+            }
+            return new SuccessResult(); //Boş bir başarılı sonuç dönüldü.
+        }
+    }
+}
