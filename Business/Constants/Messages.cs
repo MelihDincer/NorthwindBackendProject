5f0a438c06d9904c668ab8f1c242c5ea20c00428
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-28 22:28:03 +0300

    Yeni durum mesajlarının eklenmesi.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,8 +7,14 @@ namespace Business.Constants
     //Bir kere newlendikten sonra bellekte dursun ve herkes onu kullansın diye 'static' keywordü kullanıldı.
     public static class Messages
     {
-        public static string ProductAdded = "Ürün başarıyla eklendi.";
-        public static string ProductDeleted = "Ürün başarıyla silindi.";
-        public static string ProductUpdated = "Ürün başarıyla güncellendi.";
+        public static string ProductAdded = "Ürün başarıyla eklendi."; //Business.Concrete içerisindeki ProductManager'da ilk kez kullandık.
+        public static string ProductDeleted = "Ürün başarıyla silindi."; //Business.Concrete içerisindeki ProductManager'da ilk kez kullandık.
+        public static string ProductUpdated = "Ürün başarıyla güncellendi."; //Business.Concrete içerisindeki ProductManager'da ilk kez kullandık.
+        public static string UserNotFound = "Kullanıcı bulunamadı."; //Business.Concrete içerisindeki AuthManager'da ilk kez kullandık.
+        public static string PasswordError = "Şifre hatalı."; //Business.Concrete içerisindeki AuthManager'da ilk kez kullandık.
+        public static string SuccessfulLogin = "Sisteme giriş başarılı."; //Business.Concrete içerisindeki AuthManager'da ilk kez kullandık.
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut."; //Business.Concrete içerisindeki AuthManager'da ilk kez kullandık.
+        public static string UserRegistred = "Kullanıcı başarıyla kaydedildi."; //Business.Concrete içerisindeki AuthManager'da ilk kez kullandık.
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu"; //Business.Concrete içerisindeki AuthManager'da ilk kez kullandık.
     }
 }
