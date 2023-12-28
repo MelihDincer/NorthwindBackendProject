9018ec9441e97bbbf4f0036784990df2ae53d6c6
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-28 22:26:32 +0300

    JWT altyapısının kurulması ve implementasyon çalışmalarının yapılması.

---
+++ Core/Utilities/Security/Jwt/ITokenHelper.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public interface ITokenHelper
+    {
+        //Kullanıcı bilgisi ve rol bilgisinden Token oluşturma işlemi
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaims);
+    }
+}
---
+++ Core/Utilities/Security/Jwt/TokenOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class TokenOptions
+    {
+        public string Audience { get; set; } //Tokenın kullanıcı kitlesi
+        public string Issuer { get; set; } //İmzalayan
+        public int AccessTokenExpiration { get; set; } //Dakika
+        public string SecurityKey { get; set; } //Güvenlik anahtarı
+    }
+}
