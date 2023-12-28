9b0d2dbbc9e216655dee92dabae7fed4d6b0ec32
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-28 22:30:27 +0300

    HashingHelper classın oluşturulması ve password has oluşturma işlemlerinin tamamlanması.

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        //Password hash oluşturma
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) //out keywordü, parametreyi gönderdiğimizde değişen nesne aynı zamanda bizim byte arrayimize aktarılmasını sağlayacaktır.
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()) //Hashing algoritmamız
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        //Password hashin doğrulanması
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512()) //Hashing algoritmamız
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
