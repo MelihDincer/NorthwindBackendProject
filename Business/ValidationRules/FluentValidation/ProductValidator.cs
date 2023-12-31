5941a6625d12022a3175cad8941536d842a3162e
Melih DİNÇER <m_dincer41@hotmail.com>
2023-12-31 16:14:33 +0300

    ProductValidator sınıfının oluşturulması ve örnek kuralların yazılması.

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).Length(2, 30);
+            RuleFor(p => p.UnitPrice).NotEmpty();
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(1); //1'den büyük ya da Eşit.
+            RuleFor(p => p.UnitPrice).GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1); //Categori Id değeri 1 olan ürünlerin ücreti minimum 10dur.
+            RuleFor(p => p.ProductName).Must(StartWithA);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
